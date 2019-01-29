f34ab42b168589fdbae63d6767bccef0030fc366
yura <test@test.com>
2019-01-29 12:11:13 +0000

    [Studio] Reverted to "Initialize application"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,8 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
-import { homeiconclassic_PhonePortrait } from '../pages/homeiconclassic/PhonePortrait/homeiconclassic';
 export class Screens {
   static declarations = [
-    MenuComponent,
-    homeiconclassic_PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    'homeiconclassic': {
-      PhonePortrait: homeiconclassic_PhonePortrait
-    }
-  }
+  static mapping = {}
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,2 +1,1 @@
-export { mock_homeiconclassic as homeiconclassic } from './homeiconclassic/index';
 export { mock_menu as menu } from './menu/index';
