3172d9c6c02cb2403315c80fda1d5d8c843254fe
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-04-08 14:15:33 +0300

    [Studio] Update client version

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { SmartUxClient, config } from 'smartux-client';
-import { ComponentsModule } from 'components';
-import { Screens } from './app.screens';
-import { ClientApp } from './app.component';
-import { Hooks } from './app.hooks';
-
+import { NgModule } from "@angular/core";
+import { IonicApp, IonicModule } from "ionic-angular";
+import { SmartUxClient, config } from "smartux-client";
+import { ComponentsModule } from "components";
+import { Screens } from "./app.screens";
+import { ClientApp } from "./app.component";
+import { Hooks } from "./app.hooks";
+import { GlobalData } from "./globaldata.service";
 @NgModule({
-  bootstrap: [IonicApp],
-  declarations: [ClientApp, ...Screens.declarations],
-  entryComponents: Screens.declarations,
-  imports: [
-    IonicModule.forRoot(ClientApp, config.ionic),
-    SmartUxClient,
-    ComponentsModule
-  ],
-  providers: [
-    Hooks
-  ]
+    bootstrap: [IonicApp],
+    declarations: [ClientApp, ...Screens.declarations],
+    entryComponents: Screens.declarations,
+    imports: [
+        IonicModule.forRoot(ClientApp, config.ionic),
+        SmartUxClient,
+        ComponentsModule
+    ],
+    providers: [
+        Hooks,
+        GlobalData
+    ]
 })
 export class AppModule {
 }
